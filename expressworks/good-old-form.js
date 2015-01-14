c755f9f29efe3f7d730cfd666bd57268a3cfb9e5
franklingu <franklingujunchao@gmail.com>
2015-01-14 23:15:52 +0800

    upgrade express to 4.X and rerun solutions
    all are fixed for express 4

--- expressworks/good-old-form.js
+++ expressworks/good-old-form.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
-// the get method here is to pass the verification
-// but if use post, we will get the actual input--then
-// you cannot pass the verification
-app.get('/form', function (req, res) {
+// express 4.x does not have those middleware bundled by default already
+app.post('/form', function (req, res) {
   console.log(req.body);
   res.send(req.body.str.split('').reverse().join(''));
 });
