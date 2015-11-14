974c2f3fbded05e446e6c3ab0911897860dc13a5
franklingu <franklingujunchao@gmail.com>
2015-11-14 23:48:04 +0800

    added group_by in lodash

---
+++ lololodash/count_the_comments.js
@@ -0,0 +1,18 @@
+// include the Lo-Dash library
+var _ = require("lodash");
+
+var worker = function(comments) {
+    var vals = [];
+    var grouped = _.groupBy(comments, function (comment) {
+        return comment.username;
+    })
+    _.forEach(grouped, function (comments, username) {
+        vals.push({username: username, comment_count: _.size(comments)});
+    });
+    return _.sortBy(vals, function (v) {
+        return 0 - v.comment_count;
+    });
+};
+
+// export the worker function as a nodejs module
+module.exports = worker;
